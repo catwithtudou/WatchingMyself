e83c78f509de3cbc39767b36bda75fe9554c0a51
zhengyu <949812478@qq.com>
2020-12-24 11:10:18 +0800

    update the document

---
+++ test/test_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+ *@Author tudou
+ *@Date 2020/12/24
+ **/
+
+
+func TestUintNum(t *testing.T){
+	var a uint8 = 1
+	var b uint8 = 255
+	fmt.Println("result:",a+b)
+}
