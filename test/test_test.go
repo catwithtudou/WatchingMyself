847ba281f69c979b00b176e02e7d3bb77e72dcdd
zhengyu <949812478@qq.com>
2021-01-01 12:08:02 +0800

    update the distributed_system

--- test/test_test.go
+++ test/test_test.go
@@ -16,3 +16,16 @@ func TestUintNum(t *testing.T){
 	var b uint8 = 255
 	fmt.Println("result:",a+b)
 }
+
+func TestSliceMap(t *testing.T){
+	a:=make(map[string][]string)
+	b:=make([]string,0)
+	b=append(b,"1")
+	a["1"]=b
+	_,ok:=a["1"]
+	if ok{
+		a["1"]=append(a["1"],"2")
+	}
+	fmt.Println(a["1"])
+	fmt.Println(b)
+}
